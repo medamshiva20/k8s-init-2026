ec8db0c7a5301f8ea7c74f2a12809cf20497ec2c
medamshiva20 <medamshiva20@gmail.com>
2026-07-15 14:02:13 +0530

    configuring mysql image

---
+++ secret-reader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# 1. check location exist or not
+# 2. if exist, read it and export it. then call the mysql entrypoint script
+# 3. if it does not exist throw the error
+
+if [ -f /secrets/mysql-secret-reader.txt ];
+then 
+    PASSWORD=$(cat /secrets/mysql-secret-reader.txt)
+    echo "Password accessed successfully"
+else
+    echo "Can't find the secret file"
+    exit 1
+fi 
+
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+rm -rf /secrets/mysql-secret-reader.txt
+exec /usr/local/bin/docker-entrypoint.sh mysqld
